bb9fa120a5d166637d5b05b66d549a3cc9a83528
monderedwan <46160957+monderedwan@users.noreply.github.com>
2018-12-26 15:16:55 +0200

    d commit

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -5,8 +5,8 @@ package javaapplication4;
 public class JavaApplication4 {
 
     public static void main(String[] args) {
-        int a = 5 ;
-        System.out.println(a);
+        int b = 2 ;
+        System.out.println(b);
     }
     
 }
